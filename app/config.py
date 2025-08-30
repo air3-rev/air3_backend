b8eaf92ed0b58f3b9a2d5d972232f0097153d9ca
Zineb Stoa <zineb.stoa@gmail.com>
2025-08-30 17:35:22 +0200

    🎉 app setup with makefile and additional requirements

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
-from pydantic_settings import BaseSettings
-from typing import Optional, List
+from typing import List, Optional
+
 from pydantic import Field
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
@@ -8,28 +9,30 @@ class Settings(BaseSettings):
     app_name: str = "Simple Python API"
     app_version: str = "1.0.0"
     debug: bool = False
-    
+
     # Server Settings
     host: str = "0.0.0.0"
     port: int = 8000
-    
+
     # Database Settings
     database_url: Optional[str] = "sqlite:///./app.db"
-    
+
     # Supabase Settings
     supabase_url: Optional[str] = None
     supabase_anon_key: Optional[str] = None
     supabase_jwt_secret: Optional[str] = None
-    
+
     # CORS Settings
     cors_origins: List[str] = ["*"]
     cors_methods: List[str] = ["*"]
     cors_headers: List[str] = ["*"]
-    
-    #LENS API SETTINGS
+
+    # LENS API SETTINGS
     LENS_URL: str = Field(..., alias="LENS_URL")
     LENS_TOKEN: str = Field(..., alias="LENS_TOKEN")
-    
+
+    OPENAI_API_KEY: str = Field(..., alias="OPENAI_API_KEY")
+
     class Config:
         env_file = ".env"
         case_sensitive = False
